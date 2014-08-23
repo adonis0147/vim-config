1d2f7069fb3131fcfbb4a4ac7405dbf68bced193
AdonisLing <adonis0147@gmail.com>
2014-08-24 00:19:07 +0800

    Update extra

--- extra/install.sh
+++ extra/install.sh
@@ -2,11 +2,11 @@
 
 sed -i "/a.vim/, +a Bundle 'Valloric/YouCompleteMe'" ~/.vim/plugins.vim
 
+cat ./config >> ~/.vim/plugins_config.vim
+cp ./ycm_extra_conf.py ~/.vim
+
 cd ~/.vim/bundle
 git clone --recursive https://github.com/Valloric/YouCompleteMe
 
 cd ~/.vim/bundle/YouCompleteMe
 ./install.sh --clang-completer
-
-cat ./config >> ~/.vim/plugins_config.vim
-cp ./ycm_extra_conf.py ~/.vim
